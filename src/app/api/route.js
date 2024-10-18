9bdcd07c4699b8f39eed3db92852398621eb09e0
notrodes <reallystupidid@gmail.com>
2024-10-17 21:06:39 -0400

    Begin outlining

---
+++ src/app/api/route.js
@@ -0,0 +1,3 @@
+export async function GET(_request) {
+    return 'test';
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ExerciseList from "./exerciseList";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Werk</h1>
-        <button>Generate Workout</button>
+        <ExerciseList/>
       </main>
       <footer className={styles.footer}>
         
