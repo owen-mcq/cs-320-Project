5037650588e1f725dc3bfb186c608e5a0cd57007
notrodes <reallystupidid@gmail.com>
2024-10-24 11:44:23 -0400

    update

--- src/app/exerciseList.js
+++ src/app/exerciseList.js
@@ -1,11 +1,14 @@
 "use client"
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function ExerciseList({ user }) {
+export default function ExerciseList({ params }) {
     // make a request to backend and return a list of exercises
     const [json, setJSON] = useState(''); 
-    fetch("api").then(res => res.text().then(text => setJSON(text)));
+    useEffect(
+        () => {
+            fetch("api/?" + new URLSearchParams({muscle: ['dfa', 'dsd']})).then(res => res.text()).then(text => setJSON(text)) }
+    );
 
     return <p>{json}</p>;
 }
