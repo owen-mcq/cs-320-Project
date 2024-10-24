d7e03f7974885c10df097ee7508f424a1a9af237
notrodes <reallystupidid@gmail.com>
2024-10-24 11:44:23 -0400

    update

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,3 +1,13 @@
-export async function GET(_request) {
-    return new Response("Hello");
+import { NextResponse } from "next/server";
+const api = 'https://exercisedb-api.vercel.app/';
+
+export async function GET(request) {
+    // Want exercise within muscle group parameters and within time if we have time data
+    // favor exercises that the user liked
+    for (const [key, value] of request.nextUrl.searchParams) {
+        console.log(value.split(','));
+    }
+    const exercises = await (await fetch(api + `api/v1/bodyparts/${request}/exercises`)).json();
+    // console.log(request.url);
+    return NextResponse.json(exercises);
 }
