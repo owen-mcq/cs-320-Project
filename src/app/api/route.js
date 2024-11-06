3b2a73a54ddc10df5d5644d5e888d261cc2dd100
owenmcq <newomcq@gmail.com>
2024-11-06 04:57:38 -0500

    fixing branch and fixing how data is stored

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -9,7 +9,7 @@ export async function GET(request) {
   for (const [key, value] of request.nextUrl.searchParams) {
     if (key === "muscle") muscles = value.split(",");
   }
-  
+ 
   const exercises = await Promise.all(
     muscles.map(async (muscle) =>
       (await fetch(api + `api/v1/muscles/${muscle}/exercises`)).json(),
@@ -22,14 +22,26 @@ export async function GET(request) {
 export async function POST(request) {
     const workoutToSave = await request.json();
     const client = new MongoClient('mongodb://localhost:27017');
-  
+ 
     try {
-        // Connect the client
         await client.connect();
 
-        // Insert document without using a transaction
+
         const coll = client.db('workoutAppBackend').collection('workouts');
-        await coll.updateOne({date: 0}, {$set: {exercises: workoutToSave}}, {upsert: true});
+        const exercises = workoutToSave.exercises;
+
+
+        const insertPromises = workoutToSave.map(exercise =>
+            coll.updateOne(
+                { exerciseId: exercise.exerciseId },
+                { $set: exercise },
+                { upsert: true }
+            )
+        );
+
+
+        await Promise.all(insertPromises);
+
 
         console.log(await coll.findOne({date: 0}))
         console.log('Above is the inserted example of a workout');
@@ -39,6 +51,4 @@ export async function POST(request) {
         console.error(error);
         await client.close();
     }
-    // Save workoutToSave into the database
-    
 }
