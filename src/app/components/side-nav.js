02b29afea0026841fad7a309110b5c4e81d21dc2
Patrick Sun <149448279+patrsun@users.noreply.github.com>
2024-11-18 12:17:19 -0500

    feat: shadcn + sidenav (#22)
    misc: add heroicons module
    
    feat: Add sidebar components
    
    fixed some issues with conflicting keys and added sidenav to layout
    
    feat: add sidenav to ui

---
+++ src/app/components/side-nav.js
@@ -0,0 +1,23 @@
+import NavLinks from "@/app/components/nav-links";
+import WerkLogo from "@/app/components/werk-logo";
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <div className="mb-2 flex items-end justify-start rounded-md bg-black p-4 md:h-24">
+        <div className="w-32 text-white md:w-40">
+          <WerkLogo />
+        </div>
+      </div>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
-import Link from "next/link";
+import SideNav from "@/app/components/side-nav";
+
 
 export const metadata = {
   title: "Werk",
@@ -10,8 +11,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Link href="/"><h1 className="text-3xl font-bold text-center mb-8">Werk</h1></Link>
-        {children}
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
