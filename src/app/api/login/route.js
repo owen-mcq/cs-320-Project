3eb0cf8dd8ca730943472b00de28871881ceae93
notrodes <reallystupidid@gmail.com>
2024-11-18 10:40:57 -0500

    Send login to backend

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,5 +1,7 @@
-const 
+import { NextResponse } from "next/server";
 
-export async function POST() {
-    
+export async function POST(request) {
+    let data = await request.json();
+    console.log("User login by " + data.username);
+    return new NextResponse();
 }
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,9 +5,11 @@ export default function Home() {
     function handleLogin(event) {
         event.preventDefault();
         const formData = new FormData(document.getElementsByTagName("form")[0], document.getElementsByTagName('button')[0]);
-        
-        const hash = globalThis.crypto.subtle.digest('SHA-256', new ArrayBuffer(formData.password)).then(console.log);
-        console.log(hash);
+        console.log(formData)
+        const hash = globalThis.crypto.subtle.digest('SHA-256', new ArrayBuffer(formData.password))
+          .then(hash => {
+            fetch('api/login', {method: 'POST', body: JSON.stringify({ username: formData.get('username'), password: [...new Uint8Array(hash)].map(x => x.toString(16).padStart(2, '0')).join('') })})
+          });
     }
 
     return (
