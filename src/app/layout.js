16286038cd214451c60dce69381724b11dfa5ab5
= <patrsun14@gmail.com>
2024-11-19 12:11:08 -0500

    move main page to "main" endpoint

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import SideNav from "@/components/ui/side-nav";
 
 export const metadata = {
   title: "Werk",
@@ -9,11 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        
-         
-            {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
