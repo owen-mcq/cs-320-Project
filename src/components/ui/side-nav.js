be681f23bdc9cc364849b440740cda468df7c301
= <patrsun14@gmail.com>
2024-11-20 00:46:47 -0500

    fixed imports

--- src/components/ui/side-nav.js
+++ src/components/ui/side-nav.js
@@ -14,7 +14,9 @@ export default function SideNav() {
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <div className="hidden md:block">Sign Out</div>
+            <a href="/api/logout" className="hidden md:block">
+              Sign Out
+            </a>
           </button>
         </form>
       </div>
