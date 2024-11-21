cab0847040b852ad804497555417048420dccc6b
= <patrsun14@gmail.com>
2024-11-20 21:41:10 -0500

    add login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,22 +1,66 @@
-'use client'
-import "./login.css";
+"use client";
+import { FormEvent, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function Home() {
-    function handleLogin(event) {
-        event.preventDefault();
-        const formData = new FormData(document.getElementsByTagName("form")[0], document.getElementsByTagName('button')[0]);
-        console.log(formData)
-        fetch('api/login', {method: 'POST', body: JSON.stringify({ username: formData.get('username'), password: formData.get('password')})});
+export default function Login() {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+    if (res?.error) {
+      setError(res.error);
     }
+    if (res?.ok) {
+      return router.push("/");
+    }
+  };
 
-    return (
-        <main>
-          <form action="/api/login" method="POST" >
-            <input name="username" autoComplete="on" type="text" placeholder="Username"/>
-            <input name="password" autoComplete="on" type="password" placeholder="Password"/>
-            <button>Login</button>
-          </form>
-        </main>
-    );
-  }
-  
+  return (
+    <section className="w-full h-screen flex items-center justify-center">
+      <form
+        className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
+        border border-solid border-black bg-white rounded"
+        onSubmit={handleSubmit}
+      >
+        {error && <div className="text-black">{error}</div>}
+        <h1 className="mb-5 w-full text-2xl font-bold">Sign In</h1>
+        <label className="w-full text-sm">Email</label>
+        <input
+          type="username"
+          placeholder="Username"
+          className="w-full h-8 border border-solid border-black rounded p-2"
+          name="username"
+        />
+        <label className="w-full text-sm">Password</label>
+        <div className="flex w-full">
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full h-8 border border-solid border-black rounded p-2"
+            name="password"
+          />
+        </div>
+        <button className="w-full border border-solid border-black rounded">
+          Sign In
+        </button>
+
+        <Link
+          href="/register"
+          className="text-sm text-[#888] transition duration-150 ease hover:text-black"
+        >
+          Register
+        </Link>
+      </form>
+    </section>
+  );
+}
+
