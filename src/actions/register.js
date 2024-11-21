466952d7810e0e1dd912303ca8db68a9ef2371f3
= <patrsun14@gmail.com>
2024-11-20 21:39:05 -0500

    add register server action

---
+++ src/actions/register.js
@@ -0,0 +1,26 @@
+"use server";
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export async function register(values) {
+  const { username, password } = values;
+
+  try {
+    await connectDB();
+    const userFound = await User.findOne({ username });
+    if (userFound) {
+      return {
+        error: "user with username already exists!",
+      };
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      username,
+      password: hashedPassword,
+    });
+    const savedUser = await user.save();
+  } catch (error) {
+    console.log(error);
+  }
+}
