531e098bbedffd546c3d7b32aab74471927c32d7
notrodes <reallystupidid@gmail.com>
2024-12-09 09:32:32 -0500

    Finish generating based on saved profile

--- src/lib/querying.js
+++ src/lib/querying.js
@@ -1,32 +1,39 @@
 const { MongoClient } = require("mongodb");
+import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import User  from "@/models/User";
+
+async function getEquipment() {
+  const connection = await mongoose.connect("mongodb://127.0.0.1:27017/workoutAppBackend");
+  const session = await getServerSession(authOptions);
+  const username = session.user.username;
+
+  // const coll1 = client.db("workoutAppBackend").collection("users");
+  const equipment = await (await User.findOne({ username: username })).get('equipment');
+  return equipment;
+}
 
 export default async function findWorkout(bodyPartList, _excludedEquipment) {
   // bodyParts - array of strings i.e. ["waist", "chest"]
   // excludedEquipment - array of strings i.e. ["band", "barbell"]
 
-  const client = new MongoClient("mongodb://localhost:27017");
+  const client = new MongoClient("mongodb://localhost:27017/");
   console.log(bodyPartList);
   try {
     await client.connect();
-    const session = await getServerSession(authOptions);
-    const username = session.user.username;
 
-    const coll1 = client.db("workoutAppBackend").collection("users");
-    const equipment = await coll1.findOne({username:username}).equipment.toArray();
-console.log(equipment)
     // database name: workoutAppBackend 
     // collection name: workouts
     const coll = client.db("workoutAppBackend").collection("exercises");
-
+    const equipment = await getEquipment()
     const query = {
       bodyParts: { $in: bodyPartList },
-      Equipment: { $in: equipment }
+      Equipment: { $in:  equipment}
     };
 
     const workouts = await coll.find(query).limit(12).toArray();
-
+    console.log(workouts)
     // returns an array of workout documents that match the query
     // limited to a maximum of 12 exercises
     return workouts;
