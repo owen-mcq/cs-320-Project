ff6ec32c3d175a2fe5fd0a7d963d2102e571ff43
= <patrsun14@gmail.com>
2024-12-15 21:02:36 -0500

    fix data going to test db instead of actual db

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // establishes connection to local mongodb database
 export default async function connectDB() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/testDB");
+    await mongoose.connect("mongodb://127.0.0.1:27017/workoutAppBackend");
   } catch (error) {
     console.log("Database connection failed ", error);
   }
