66fa4781a0ffddf15cee46131d06186d3c2c2a1c
= <patrsun14@gmail.com>
2024-12-15 20:44:57 -0500

    add body parts data file
    - contains parts and icons to go with the parts

---
+++ src/data/body-parts.js
@@ -0,0 +1,56 @@
+const upperBody = [
+  {
+    name: "chest",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-03-1024.png",
+  },
+  {
+    name: "shoulders",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-04-1024.png",
+  },
+  {
+    name: "upper arms",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-05-1024.png",
+  },
+  {
+    name: "lower arms",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-18-1024.png",
+  },
+  {
+    name: "back",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-11-1024.png",
+  },
+];
+
+const lowerBody = [
+  {
+    name: "waist",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-24-1024.png",
+  },
+  {
+    name: "upper legs",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-14-1024.png",
+  },
+  {
+    name: "lower legs",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-21-1024.png",
+  },
+];
+
+const other = [
+  {
+    name: "cardio",
+    icon: "https://cdn2.iconfinder.com/data/icons/gym-workout-equipments/279/gym-workout-equipment-002-1024.png",
+  },
+  {
+    name: "neck",
+    icon: "https://cdn3.iconfinder.com/data/icons/muscles/512/muscle-anatomy-human-02-1024.png",
+  },
+];
+
+const bodyParts = {
+  upperBody,
+  lowerBody,
+  other,
+};
+
+export default bodyParts;
