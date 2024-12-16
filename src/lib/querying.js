ddd3243cf2647baace6cfdab724ca868818ff94d
= <patrsun14@gmail.com>
2024-12-15 20:52:15 -0500

    feat: add UI components for exercise generation

--- src/lib/querying.js
+++ src/lib/querying.js
@@ -2,20 +2,23 @@ const { MongoClient } = require("mongodb");
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import User  from "@/models/User";
+import User from "@/models/User";
 
 async function getEquipment() {
-  const connection = await mongoose.connect("mongodb://127.0.0.1:27017/workoutAppBackend");
+  const connection = await mongoose.connect(
+    "mongodb://127.0.0.1:27017/workoutAppBackend",
+  );
   const session = await getServerSession(authOptions);
   const username = session.user.username;
 
   // const coll1 = client.db("workoutAppBackend").collection("users");
-  const equipment = await (await User.findOne({ username: username })).get('equipment');
+  const equipment = await (
+    await User.findOne({ username: username })
+  ).get("equipment");
   return equipment;
 }
 
 export default async function findExercise(bodyPartList, _includedEquipment) {
-
   // bodyParts - array of strings i.e. ["waist", "chest"]
   // includedEquipment - array of strings i.e. ["band", "barbell"]
 
@@ -27,17 +30,13 @@ export default async function findExercise(bodyPartList, _includedEquipment) {
     // Database name: workoutAppBackend
     // Collection name: exercises
     const coll = client.db("workoutAppBackend").collection("exercises");
-    const equipment = await getEquipment()
+    const equipment = await getEquipment();
 
     // const workouts = await coll.find(query).limit(12).toArray();
     // console.log(workouts)
     // // returns an array of workout documents that match the query
     // // limited to a maximum of 12 exercises
     // return workouts;
-
-    const exerciseCount = 12;
-    const perBodyPart = Math.floor(exerciseCount / bodyPartList.length);
-    const remaining = exerciseCount % bodyPartList.length;
 
     const exerciseCount = 12;
     const perBodyPart = Math.floor(exerciseCount / bodyPartList.length);
@@ -62,7 +61,6 @@ export default async function findExercise(bodyPartList, _includedEquipment) {
     exerciseList = exerciseList.sort(() => Math.random() - 0.5);
 
     return exerciseList;
-
   } catch (error) {
     console.error(error);
     throw new Error("Failed to fetch workouts");
@@ -70,8 +68,6 @@ export default async function findExercise(bodyPartList, _includedEquipment) {
     await client.close();
   }
 }
-
-
 
 // async function storeWorkout(exercises) {
 //   "use server";
@@ -97,3 +93,4 @@ export default async function findExercise(bodyPartList, _includedEquipment) {
 //   }
 
 // }
+
